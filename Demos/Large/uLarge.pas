ac0df0603d7dce234cf5abdceb6e063c1dd87f42
Laurent Meyer <Git@ea4d.com>
2025-06-16 10:54:03 +0200

    Add Memory usage to "Large" demo.

--- Demos/Large/uLarge.pas
+++ Demos/Large/uLarge.pas
@@ -102,9 +102,24 @@ implementation
 uses
     System.Diagnostics
     , uJX4YAML
+    , Windows
+    , PSApi
   ;
 
 {$R *.fmx}
+
+
+function GetMemoryUsed: SIZE_T;
+var
+  pmc: PROCESS_MEMORY_COUNTERS;
+begin
+  pmc.cb := SizeOf(pmc);
+  if GetProcessMemoryInfo(GetCurrentProcess, @pmc, pmc.cb) then
+    Result := pmc.WorkingSetSize
+  else
+    Result := 0;
+end;
+
 
 procedure TForm4.ButtonClick( Sender : TObject );
   var
@@ -113,14 +128,19 @@ procedure TForm4.ButtonClick( Sender : TObject );
     LWatch : TStopWatch;
     LJSize: Int64;
 begin
+
     Memo1.Lines.Clear;
+
+    var MB := GetMemoryUsed div (1024*1024);
+    Memo1.Lines.add( Format( 'Used Memory %d MB', [ MB ] ) );
 
     LWatch := TStopWatch.StartNew;
     Memo1.Lines.add( 'Loading ebay''s "aspects" json file :' );
   LJSize := TJX4Object.LoadFromFile('aspects100.json', LJsonStr);
     Memo1.Lines.add( Format( '  Stream size: %n KB', [ (LJSize / 1024) ] ));
     Memo1.Lines.add(Format('==> %d ms', [ LWatch.ElapsedMilliseconds ]));
-
+    MB := GetMemoryUsed div (1024*1024);
+    Memo1.Lines.add( Format( 'Used Memory %d MB', [ MB ] ) );
 
     Memo1.Lines.add( '' );
     Memo1.Lines.add( 'Convert Json String to JSX4 Objects (Deserialize):' );
@@ -128,7 +148,8 @@ begin
   LJObj := TJX4Object.FromJSON<TfetchItemAspectsContentType>(LJsonStr, [ joRaiseException] );
     Memo1.Lines.add(Format('==> %d ms', [ LWatch.ElapsedMilliseconds ]));
     Memo1.Lines.add(Format('==> %n MB/s', [(LJSize / (1024*1000)) / (LWatch.ElapsedMilliseconds / 1000)]));
-
+    MB := GetMemoryUsed div (1024*1024);
+    Memo1.Lines.add( Format( 'Used Memory %d MB', [ MB ] ) );
 
     Memo1.Lines.add( '' );
     Memo1.Lines.add( 'JSX4 Object Cloning (by RTTI):' );
@@ -136,7 +157,8 @@ begin
   LJObjClone := LJObj.Clone<TfetchItemAspectsContentType>;
     Memo1.Lines.add(Format('==> %d ms', [ LWatch.ElapsedMilliseconds ]));
     Memo1.Lines.add(Format('==> %n MB/s', [(LJSize /  (1024*1000)) / (LWatch.ElapsedMilliseconds / 1000)]));
-
+    MB := GetMemoryUsed div (1024*1024);
+    Memo1.Lines.add( Format( 'Used Memory %d MB', [ MB ] ) );
 
     Memo1.Lines.add( '' );
     Memo1.Lines.add( 'JSX4 Object Cloning (by Merging):' );
@@ -145,6 +167,8 @@ begin
   LJObjMerge.Merge(LJObjClone, [ jmoAdd ]);
     Memo1.Lines.add(Format('==> %d ms', [ LWatch.ElapsedMilliseconds ]));
     Memo1.Lines.add(Format('==> %n KM/s', [(LJSize /  (1024*1000)) / (LWatch.ElapsedMilliseconds / 1000)]));
+    MB := GetMemoryUsed div (1024*1024);
+    Memo1.Lines.add( Format( 'Used Memory %d MB', [ MB ] ) );
 
     Memo1.Lines.add( '' );
     Memo1.Lines.add( 'Revert JX4 Objects to Json String (Serialize)):' );
@@ -152,12 +176,16 @@ begin
   LJsonStr := LJObj.ToJson([ joNullToEmpty] );
     Memo1.Lines.add(Format('==> %d ms', [ LWatch.ElapsedMilliseconds ]));
     Memo1.Lines.add(Format('==> %n MB/s', [(LJSize / (1024*1000)) / (LWatch.ElapsedMilliseconds / 1000)]));
+    MB := GetMemoryUsed div (1024*1024);
+    Memo1.Lines.add( Format( 'Used Memory %d MB', [ MB ] ) );
 
     Memo1.Lines.add( '' );
     Memo1.Lines.add( 'YAMLize' );
     LWatch := TStopWatch.StartNew;
     var YAMLstr := LJObj.ToYAML;
     Memo1.Lines.add(Format('==> %d ms', [ LWatch.ElapsedMilliseconds ]));
+    MB := GetMemoryUsed div (1024*1024);
+    Memo1.Lines.add( Format( 'Used Memory %d MB', [ MB ] ) );
 
     Memo1.Lines.add( '' );
     LWatch := TStopWatch.StartNew;
@@ -166,6 +194,8 @@ begin
   TJX4Object.SaveToYAMLFile( 'jsx4.yaml', YAMLstr);
     Memo1.Lines.add( Format( '  Stream size: %n KB', [ (LJSize / 1024) ] ));
     Memo1.Lines.add(Format('==> %d ms', [ LWatch.ElapsedMilliseconds ]));
+    MB := GetMemoryUsed div (1024*1024);
+    Memo1.Lines.add( Format( 'Used Memory %d MB', [ MB ] ) );
 
     Memo1.Lines.add( '' );
     LWatch := TStopWatch.StartNew;
@@ -174,6 +204,9 @@ begin
   LJObjClone.Free;
   LJObjMerge.Free;
     Memo1.Lines.add( Format( '  Freed in %d ms', [ LWatch.ElapsedMilliseconds ] ) );
+    MB := GetMemoryUsed div (1024*1024);
+    Memo1.Lines.add( Format( 'Used Memory %d MB', [ MB ] ) );
+
   end;
 
 end.
